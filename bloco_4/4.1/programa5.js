1d7785087536bb028221582785c44a62660b0f74
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-10-23 19:26:13 -0300

    add exercicio 5 - obg jessandro

---
+++ bloco_4/4.1/programa5.js
@@ -0,0 +1,12 @@
+let anguloA = 40;
+let anguloB = 100;
+let anguloC = 40;
+
+let somaAngulos = anguloA + anguloB + anguloC;
+
+triangulo = (somaAngulos === 180) ? "true":"false";
+
+console.log(triangulo);
+
+
+
