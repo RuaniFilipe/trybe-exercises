55b5209eac4d4d5870eed0d7e33fe19f3661a298
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-10-23 23:10:37 -0300

    add exercicio 7

---
+++ bloco_4/4.1/programa7.js
@@ -0,0 +1,16 @@
+let nota = 95;
+
+if (nota >= 0 && nota < 50){
+    console.log("F");    
+} else if (nota < 60){
+    console.log("E");
+} else if (nota < 70){
+    console.log("D");
+} else if (nota < 80){
+    console.log("C");
+} else if (nota < 90){
+    console.log("B")
+} else if (nota < 100){
+    console.log("A")
+} else
+    console.log("Confira sua nota! Nota Inválida!")
